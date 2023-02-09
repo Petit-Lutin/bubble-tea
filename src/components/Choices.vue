8b60913f59d4dee58b6e086fc6cf71c4b93c1b42
Petit-Lutin <laurence.platzer@wanadoo.fr>
2023-02-09 15:36:24 +0100

    fresh install, use of Node.js instead of CDN

---
+++ src/components/Choices.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      picked: 'One'
+    }
+  }
+}
+</script>
+
+<template>
+	<div>Picked: {{ picked }}</div>
+
+	<input type="radio" id="one" value="One" v-model="picked" />
+	<label for="one">One</label>
+
+	<input type="radio" id="two" value="Two" v-model="picked" />
+	<label for="two">Two</label>
+</template>
